6266622ab255f67be553d0b8d19fc58231205f86
David Aslan <davidaslan112671@live.se>
2022-10-09 15:44:59 +0200

    azure start

--- IotSysDevUi/MVVM/ViewModels/KitchenViewModel.cs
+++ IotSysDevUi/MVVM/ViewModels/KitchenViewModel.cs
@@ -1,19 +1,76 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using IotSysDevUi.MVVM.Models;
+using Microsoft.Azure.Devices;
 
 namespace IotSysDevUi.MVVM.ViewModels;
 
-public class KitchenViewModel
+internal class KitchenViewModel : ObservableObject
 {
+    private readonly RegistryManager registryManager = RegistryManager.CreateFromConnectionString("HostName=SharedHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=MAZ7jkUzHFnph4OvNsuvjcJxQLS0dcHRa7TV5g0/Rzw=");
+  
+
+    public KitchenViewModel()
+    {
+        Devices = new();
+        GetDevices().ConfigureAwait(false);
+    }
+   
+
     public string Title { get; set; } = "Kitchen";
     public string Temperature { get; set; } = "23 °C";
     public string Humidity { get; set; } = "34 %";
 
-    public List<DeviceItem> Devices { get; set; } = new()
+    public List<DeviceItem> Devices { get; set; } = new();
+  
+
+    private async Task GetDevices()
     {
-    new DeviceItem{DeviceId="Device 1", DeviceName="IntelliLight", DeviceType="Light", StateActive="ON",StateInActive="OFF" },
-        new DeviceItem { DeviceId = "Device 2", DeviceName = "IntelliFan", DeviceType = "Fan", StateActive = "ON", StateInActive = "OFF" },
-        new DeviceItem { DeviceId = "Device 3", DeviceName = "IntelliLedstrip", DeviceType = "Led strip", StateActive = "ON", StateInActive = "OFF" }
-    };
+        var result= registryManager.CreateQuery("SELECT * FROM devices");
+        if (result.HasMoreResults)
+        {
+            foreach (var device in await result.GetNextAsTwinAsync())
+            {
+                var deviceItem = new DeviceItem()
+                {
+                    DeviceId = device.DeviceId
+                };
+
+                try
+                {
+                    deviceItem.DeviceName = (string) device.Properties.Reported["deviceName"];
+                }
+                catch{}
+
+                switch (deviceItem.DeviceType.ToLower())
+                {
+                    case "fan":
+                        deviceItem.IconActive = "\uf863";  //these are c# icon names
+                        deviceItem.IconInActive = "\uf863";
+                        deviceItem.StateActive = "ON";
+                        deviceItem.StateInActive = "OFF";
+                        break;
+                    case "light":
+                        deviceItem.IconActive = "\uf672";
+                        deviceItem.IconInActive = "\uf0eb";
+                        deviceItem.StateActive = "ON";
+                        deviceItem.StateInActive = "OFF";
+                        break;
+
+                    default:
+                        deviceItem.IconActive = "\uf2db";
+                        deviceItem.IconInActive = "\uf2db";
+                        deviceItem.StateActive = "ENABLE";
+                        deviceItem.StateInActive = "DISABLE";
+                        break;
+                }
+                Devices.Add(deviceItem);
+            }
+
+            
+        }
+
+        OnPropertyChanged();
+    }
 
 }
