f46ccad6941030637e86ac1494f8aa310f529e0b
David Aslan <davidaslan112671@live.se>
2022-10-11 20:07:44 +0200

    lektion 9 done

--- IotSysDevUi/MVVM/Views/KitchenView.xaml.cs
+++ IotSysDevUi/MVVM/Views/KitchenView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using IotSysDevUi.MVVM.Models;
+using Microsoft.Azure.Devices;
 
 namespace IotSysDevUi.MVVM.Views
 {
@@ -20,9 +22,61 @@ namespace IotSysDevUi.MVVM.Views
     /// </summary>
     public partial class KitchenView : UserControl
     {
+        private readonly RegistryManager registryManager = RegistryManager.CreateFromConnectionString("HostName=SharedHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=MAZ7jkUzHFnph4OvNsuvjcJxQLS0dcHRa7TV5g0/Rzw=");
+        private List<DeviceItem> devices = new List<DeviceItem>();
         public KitchenView()
         {
             InitializeComponent();
+        }
+
+        private async Task<List<DeviceItem>> GetDevices()
+        {
+            var _devices = new List<DeviceItem>();
+            var result = registryManager.CreateQuery("SELECT * FROM devices");
+            if (result.HasMoreResults)
+            {
+                foreach (var device in await result.GetNextAsTwinAsync())
+                {
+                    var deviceItem = new DeviceItem()
+                    {
+                        DeviceId = device.DeviceId
+                    };
+
+                    try
+                    {
+                        deviceItem.DeviceName = (string)device.Properties.Reported["deviceName"];
+                    }
+                    catch { }
+
+                    switch (deviceItem.DeviceType.ToLower())
+                    {
+                        case "fan":
+                            deviceItem.IconActive = "\uf863";  //these are c# icon names
+                            deviceItem.IconInActive = "\uf863";
+                            deviceItem.StateActive = "ON";
+                            deviceItem.StateInActive = "OFF";
+                            break;
+                        case "light":
+                            deviceItem.IconActive = "\uf672";
+                            deviceItem.IconInActive = "\uf0eb";
+                            deviceItem.StateActive = "ON";
+                            deviceItem.StateInActive = "OFF";
+                            break;
+
+                        default:
+                            deviceItem.IconActive = "\uf2db";
+                            deviceItem.IconInActive = "\uf2db";
+                            deviceItem.StateActive = "ENABLE";
+                            deviceItem.StateInActive = "DISABLE";
+                            break;
+                    }
+                    _devices.Add(deviceItem);
+                }
+
+
+            }
+
+            return _devices;
         }
     }
 }
