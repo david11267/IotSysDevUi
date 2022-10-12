1e306b8153de1bd2b7a06d8cc2c1d105cca8bbc6
David Aslan <davidaslan112671@live.se>
2022-10-12 14:20:04 +0200

    PRE COPY

--- IotSysDevUi/MVVM/ViewModels/KitchenViewModel.cs
+++ IotSysDevUi/MVVM/ViewModels/KitchenViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using IotSysDevUi.MVVM.Models;
 using Microsoft.Azure.Devices;
@@ -7,8 +8,22 @@ namespace IotSysDevUi.MVVM.ViewModels;
 
 internal class KitchenViewModel
 {
+    private readonly ObservableCollection<DeviceItem> _deviceItems;
+    private readonly RegistryManager registryManager = RegistryManager.CreateFromConnectionString("HostName=SharedHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=MAZ7jkUzHFnph4OvNsuvjcJxQLS0dcHRa7TV5g0/Rzw=");
+
+    public KitchenViewModel()
+    {
+        _deviceItems = new ObservableCollection<DeviceItem>();
+        
+    }
     public string Title { get; set; } = "Kitchen";
     public string Temperature { get; set; } = "23 °C";
     public string Humidity { get; set; } = "34 %";
 
+    public IEnumerable<DeviceItem> DeviceItems => _deviceItems;
+
+    private async Task PopulateDeviceItems()
+    {
+        var result = registryManager.CreateQuery("SELECT * FROM DEVICES"); 
+    }
 }
