e163c2a45b9511d7cdd300c23a9374d80a4c686a
David Aslan <davidaslan112671@live.se>
2022-10-19 22:12:55 +0200

    init

---
+++ SmartBuildingEfficeintEnergy/SmartBuildingEfficeintEnergy/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartBuildingEfficeintEnergy;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ SmartBuildingEfficeintEnergy/SmartBuildingEfficeintEnergy/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SmartBuildingEfficeintEnergy;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
